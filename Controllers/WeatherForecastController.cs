4b0d050b5765888c8a0e2b56f62695efb1c8303c
Jeferson Job Ribeiro dos Santos <jeferson.job@outlook.com>
2020-07-15 00:57:44 -0300

    É pra dar erro

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace webapi_cd_ci.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching",a
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
